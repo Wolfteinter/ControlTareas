de7895e7c70588bfff6c45805d463d741ef78b9b
Wolfteinter <onderfra@gmail.com>
2018-08-06 20:05:47 -0500

    Cambio en la conexion

--- conf/conexion.php
+++ conf/conexion.php
@@ -1,23 +1,21 @@
 <?php
-    include("conexionInicial.php");
+$server = "localhost";
+$user = "root";
+$pass = "";
+$databaseInicial = "task_controller";
 
-    $consultaInicial = "SELECT nombre FROM actual";
-    $resultado = mysqli_query($enlaceInicial, $consultaInicial);
-    $row = mysqli_fetch_assoc($resultado);
-    $database=$row['nombre'];
+$enlaceInicial = new mysqli($server, $user, $pass, $databaseInicial);
+if($enlaceInicial->connect_errno) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
 
-    $enlace = new mysqli($server, $user, $pass, $database);
-    if($enlace->connect_errno) {
-        printf("Connect failed: %s\n", mysqli_connect_error());
-        exit();
-    }
+// Poder ver los warnings, '0' <=> '1'
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 
-    // Poder ver los warnings, '0' <=> '1'
-    error_reporting(E_ALL);
-    ini_set('display_errors', '1');
-
-    // Definir el juego de carácteres con el que se trabaja
-    $charset = "utf8";
-    mysqli_set_charset ($enlace , $charset);
+// Definir el juego de carácteres con el que se trabaja
+$charset = "utf8";
+mysqli_set_charset ($enlaceInicial , $charset);
 
 ?>
